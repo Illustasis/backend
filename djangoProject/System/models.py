ba91e82636f14a9104b00a4fc3afa55e637a3192
yjl <ouroboros.sung@foxmail.com>
2022-05-28 10:54:26 +0800

    ll

--- djangoProject/System/models.py
+++ djangoProject/System/models.py
@@ -28,6 +28,17 @@ class Movie(models.Model):
     score = models.DecimalField(max_digits=1, decimal_places=1)  # 评分，5分满分，1位小数
     heat = models.IntegerField(default=0)  # 点击量
 
+class Tele(models.Model):
+    tele_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=80)
+    image: str = models.CharField(max_length=200)  # 封面图片
+    nation = models.CharField(max_length=80)
+    actor = models.CharField(max_length=200)
+    year = models.IntegerField(default=1990)
+    introduction = models.TextField(max_length=1000,default='')
+    score = models.DecimalField(max_digits=1, decimal_places=1)  # 评分，5分满分，1位小数
+    heat = models.IntegerField(default=0)  # 点击量
+
 
 class Score(models.Model):  # 评分表
     user_id = models.IntegerField
